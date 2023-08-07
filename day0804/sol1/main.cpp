bf1270cdb643d6f24648de03542c4cde13bc65bb
gyubong_park <gyubong_park@tmaxsoft.co.kr>
2023-08-07 13:04:34 +0900

    0807 solved

--- day0804/sol1/main.cpp
+++ day0804/sol1/main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -12,12 +13,21 @@ long long MAX = -1;
 // 0 - +
 // 1 - -
 // 2 - *
-char[]
 
+long long calculate(long long int &a, long long int &b, char &op) {
+    if(op == '+'){
+        return a + b;
+    }
+    else if(op == '-'){
+        return a - b;
+    }else if(op == '*'){
+        return a * b;
+    }
+}
 long long solution(string expression) {
     long long answer = 0;
 
-    std::vector<int> nums;
+    std::vector<long long> nums;
     std::vector<char> op;
     std::string temp_num;
     // vec에 수자와 연산 나누기
@@ -34,11 +44,37 @@ long long solution(string expression) {
     nums.push_back(stoi(temp_num));
 
     // 모든 조합으로 계산
-    recursive(nums, op);
+    vector<int> comb = {0, 1, 2};
+    string operator_values = "+-*";
+
+    do{
+        vector<long long> temp_nums = nums;
+        std::vector<char> temp_op = op;
+
+        for(int i=0; i < comb.size(); i++){
+            for(int j =0; j < temp_op.size();){
+                if(temp_op[j] == operator_values[comb[i]]){
+                    auto res = calculate(temp_nums[j],temp_nums[j+1], temp_op[j]);
+
+                    temp_nums.erase(temp_nums.begin()+j);
+                    temp_nums.erase(temp_nums.begin()+j);
+                    temp_nums.insert(temp_nums.begin()+j, res);
+
+                    temp_op.erase(temp_op.begin()+j);
+                }else{
+                    j++;
+                }
+            }
+        }
+
+        answer = max(answer, abs(temp_nums[0]));
+    }while(next_permutation(comb.begin(), comb.end()));
 
 
     return answer;
 }
+
+
 
 int main() {
 
@@ -51,6 +87,17 @@ int main() {
 
 
     cout << rtn << endl;
+
+
+
+/*    vector<int> comb = {0, 1, 2};
+    do{
+
+        for(auto item : comb){
+            cout << item << " ";
+        }
+        cout << endl;
+    }while(next_permutation(comb.begin(), comb.end()));*/
 
     return 0;
 }
